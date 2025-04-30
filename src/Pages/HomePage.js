e110620f5580f9e14ca8db77a8e674e45ea3bcee
Ian Jordan <ian12009@gmail.com>
2025-04-30 17:05:21 -0400

    temp portfolio moved for comparison

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,49 +1,34 @@
 import React from 'react';
-import {faGithub} from '@fortawesome/free-brands-svg-icons';
-import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import {Link } from 'react-router-dom';
-import {motion} from 'framer-motion';
-import Particle from '../Components/Particle';
-import Cube from '../Components/Cube';
 
 
-function HomePage(){
+function HomePage() {
     return (
-        
-        
-        
-        <motion.div initial={{opacity: 0}} animate={{opacity:1 }} exit={{opacity: 0}}>
-            <div>
-            <Particle />
-            </div>
 
-            <div className="HomePage">
-                <div style={{display: 'flex', justifyContent: 'center', marginBottom:'4%'}}>
-                    <Cube />
-                </div>
-                <header className="hero">
-                    <h1 className="hero-text">
-                        Hi, I am 
-                        <span> Ian Jordan.</span>
-                    </h1>
-                    <p className="h-sub-text">
-                        Welcome to my Web Development portfolio.
-                    </p>
-                    
-                    <div className="icons">
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hi, I am
+                    <span> Ian Jordan.</span>
+                </h1>
+                <p className="h-sub-text">
+                    Welcome to my Web Development portfolio.
+                </p>
+
+                <div className="icons">
                     <a href="https://github.com/ian-a-jordan" target="_blank" rel="noreferrer" className="icon-holder">
-                        
-                        <FontAwesomeIcon icon={faGithub} className="icon gh"/>
-                        </a>
-                        <a href="https://www.linkedin.com/in/ian-jordan-aa6ab8207/" target="_blank" rel="noreferrer" className="icon-holder">
-                        <FontAwesomeIcon icon={faLinkedin} className="icon li"/>
-                        </a>
-                        
-                    </div>
-                </header>
-            </div>
-        </motion.div>
+
+                        <FontAwesomeIcon icon={faGithub} className="icon gh" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/ian-jordan-aa6ab8207/" target="_blank" rel="noreferrer" className="icon-holder">
+                        <FontAwesomeIcon icon={faLinkedin} className="icon li" />
+                    </a>
+
+                </div>
+            </header>
+        </div>
     )
 }
 
